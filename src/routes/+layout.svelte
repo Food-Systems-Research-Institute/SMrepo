748e383e10746474014e162be25a6c5c0aa3c582
Christopher.Donovan <christopher.donovan@uvm.edu>
2026-05-07 09:44:21 -0400

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import '../app.css';
-  import favicon from '$lib/assets/favicon.svg';
+  import favicon from '$lib/assets/fsri-spirit-mark.svg';
   import Navbar from './Navbar.svelte';
   import Footer from './Footer.svelte';
 
