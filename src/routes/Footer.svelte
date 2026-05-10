41a03e67e51feaca6f126a373a348fed5c75da29
cdonovan <ctudonovan@gmail.com>
2026-05-10 12:00:40 -0400

    mobile collapse nav

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,31 +1,16 @@
 <script>
-  import { base } from '$app/paths';
+  import { resolve } from '$app/paths';
   import logo from '$lib/assets/fsri_logo.png';
-  import spirit from '$lib/assets/FSRI-SpiritMarks_FSRI-mark-white.png';
 </script>
 
-<footer class="footer sm:footer-horizontal bg-primary text-neutral-content items-center p-4">
+<footer
+  class="footer footer-horizontal bg-primary text-neutral-content items-center rounded-b-md p-4 md:rounded-none"
+>
   <aside class="grid-flow-col items-center">
-    <a class="text-xl" href="{base}/">
+    <a class="text-xl" href={resolve('/')}>
       <img src={logo} alt="Food Systems Research Institute" class="h-10" />
     </a>
   </aside>
-  <!-- <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-    <a
-      href="https://www.github.com/Food-Systems-Research-Institute"
-      target="_blank"
-      aria-label="GitHub"
-    >
-      <i class="fa-brands fa-github text-3xl"></i>
-    </a>
-    <a
-      href="https://www.uvm.edu/ovpr/food-systems-research/about-fsri"
-      target="_blank"
-      aria-label="FSRI"
-    >
-      <img src={spirit} alt="SM Repository" class="h-8" />
-    </a>
-  </nav> -->
 </footer>
 
 <style>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -5,12 +5,25 @@
   import { page } from '$app/state';
 </script>
 
-<div class="navbar bg-primary h-(--header-height) shadow-sm">
-  <div class="mx-auto flex w-full max-w-(--max-page-width-table) items-center">
-    <div class="navbar-start">
-      <div class="dropdown">
+<div id="test" class="w-full rounded-none rounded-t-lg shadow-sm max-lg:collapse md:rounded-none">
+  <!-- Checkbox for mobile hamburger nav toggle -->
+  <input id="navbar-1-toggle" class="peer hidden" type="checkbox" />
+  <label for="navbar-1-toggle" class="fixed inset-0 hidden max-lg:peer-checked:block"></label>
+
+  <!-- Actual header -->
+  <div class="collapse-title navbar bg-primary h-(--navbar-height) shadow-sm">
+    <div class="mx-auto flex w-full max-w-(--max-page-width-table) items-center">
+      <!-- Left block -->
+      <div class="navbar-start">
+        <a class="hidden text-xl md:block" href={resolve('/')}>
+          <img
+            src={logo}
+            alt="Food Systems Research Institute"
+            class="fsri-logo h-5 shrink md:h-8 lg:h-10"
+          />
+        </a>
         <!-- Mobile hamburger -->
-        <div tabindex="0" role="button" class="btn btn-ghost md:hidden">
+        <label for="navbar-1-toggle" class="btn btn-ghost md:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-8 w-8"
@@ -25,13 +38,12 @@
               d="M4 6h16M4 12h8m-8 6h16"
             />
           </svg>
-        </div>
+        </label>
+      </div>
 
-        <!-- Mobile Dropdown Nav -->
-        <ul
-          tabindex="-1"
-          class="menu menu-sm dropdown-content rounded-box z-1 mt-3 w-42 border bg-(--light-gray) p-2 shadow"
-        >
+      <!-- Desktop Nav -->
+      <div class="navbar-center hidden md:flex">
+        <ul class="menu menu-horizontal px-1">
           <li><a href={resolve('/')} aria-current={page.url.pathname == resolve('/')}>Home</a></li>
           <li>
             <a href={resolve('/papers')} aria-current={page.url.pathname == resolve('/papers')}
@@ -46,63 +58,62 @@
         </ul>
       </div>
 
-      <a class="hidden text-xl md:block" href={resolve('/')}>
-        <img
-          src={logo}
-          alt="Food Systems Research Institute"
-          class="fsri-logo h-5 shrink md:h-8 lg:h-10"
-        />
-      </a>
+      <!-- Right buttons -->
+      <div class="navbar-end gap-4 px-4">
+        <a
+          href="https://www.github.com/Food-Systems-Research-Institute"
+          target="_blank"
+          aria-label="GitHub"
+        >
+          <i class="fa-brands fa-github text-3xl"></i>
+        </a>
+        <a
+          href="https://www.uvm.edu/ovpr/food-systems-research/about-fsri"
+          target="_blank"
+          aria-label="FSRI"
+        >
+          <img src={spirit} alt="SM Repository" class="h-8" />
+        </a>
+      </div>
     </div>
+  </div>
 
-    <!-- Desktop Nav -->
-    <div class="navbar-center hidden md:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a href={resolve('/')} aria-current={page.url.pathname == resolve('/')}>Home</a></li>
-        <li>
-          <a href={resolve('/papers')} aria-current={page.url.pathname == resolve('/papers')}
-            >Papers</a
-          >
-        </li>
-        <li>
-          <a href={resolve('/datasets')} aria-current={page.url.pathname == resolve('/datasets')}
-            >Datasets</a
-          >
-        </li>
-      </ul>
-    </div>
-
-    <!-- Right buttons -->
-    <div class="navbar-end gap-4 px-4">
-      <a
-        href="https://www.github.com/Food-Systems-Research-Institute"
-        target="_blank"
-        aria-label="GitHub"
-      >
-        <i class="fa-brands fa-github text-3xl"></i>
-      </a>
-      <a
-        href="https://www.uvm.edu/ovpr/food-systems-research/about-fsri"
-        target="_blank"
-        aria-label="FSRI"
-      >
-        <img src={spirit} alt="SM Repository" class="h-8" />
-      </a>
-    </div>
+  <!-- Mobile expand nav -->
+  <div class="collapse-content z-1 md:hidden">
+    <hr />
+    <ul class="menu">
+      <li>
+        <a href={resolve('/')} aria-current={page.url.pathname == resolve('/')}>Home</a>
+      </li>
+      <li>
+        <a href={resolve('/papers')} aria-current={page.url.pathname == resolve('/papers')}
+          >Papers</a
+        >
+      </li>
+      <li>
+        <a href={resolve('/datasets')} aria-current={page.url.pathname == resolve('/datasets')}
+          >Datasets</a
+        >
+      </li>
+    </ul>
   </div>
 </div>
 
 <style>
   /* Mobile */
   .btn-ghost:active {
-    background-color: color-mix(in oklch, var(--color-primary) 75%, black);
+    background-color: var(--primary-dark);
     border: none;
   }
-  .dropdown-content {
+  .collapse-content {
     a {
-      color: var(--black);
-      font-size: 1.25rem;
+      color: var(--color-primary-content);
+      font-size: var(--menu-font-size-mobile);
     }
+    background-color: var(--color-primary);
+  }
+  hr {
+    border-top: 1px solid var(--color-primary-content);
   }
 
   /* Desktop */
@@ -110,14 +121,13 @@
   li {
     color: var(--color-primary-content);
     text-decoration: none;
-    font-size: 1rem;
     transition: var(--transition);
   }
   a[aria-current='true'] {
     text-decoration: underline;
   }
   li > a:hover {
-    background-color: color-mix(in oklch, var(--color-primary) 75%, black);
+    background-color: var(--primary-dark);
     color: color-mix(in oklch, var(--color-secondary-content) 80%, black);
   }
 </style>
